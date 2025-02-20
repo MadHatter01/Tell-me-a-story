718dc9eeb2e8f1752086b86b2b4cfb9543e9242d
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-02-20 16:32:38 -0500

    user name added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, emit
 import random
 
 
@@ -16,16 +16,23 @@ def home():
 def available_rooms():
     return render_template("available-rooms.html", rooms = rooms)
 
-@app.route('/room/<room_id>')
-def room(room_id):
-    print(rooms)
-    return render_template('room.html', room_id=room_id, story=rooms[room_id]['story'])
+# @app.route('/room/<room_id>')
+# def room(room_id):
+#     print('triggered')
+#     return render_template('room.html', room_id=room_id, story=rooms[room_id]['story'])
 
+@app.route('/room/<room_id>', methods=['POST'])
+def submit_username(room_id):
+    username = request.form.get('username') 
+
+    if room_id not in rooms:
+        return jsonify(error="Room not found"), 404
+    return render_template('room.html', username=username, room_id=room_id, story=rooms[room_id]['story'])
 
 @app.route('/create-room')
 def create_room():
     room_id = str(random.randint(100, 999))
-    rooms[room_id] = {'story':[], 'current':0, 'turns':[]}
+    rooms[room_id] = {'story':[]}
     return render_template('room-linker.html', room_id=room_id)
 
 
@@ -38,8 +45,23 @@ def submit_line(room_id):
         return jsonify(error="Room not found"), 404
     
     rooms[room_id]['story'].append(line)
-
+    socketio.emit('update_story', {'story': rooms[room_id]['story']}, room=room_id)
     return render_template('story.html', story=rooms[room_id]['story'])
+
+@socketio.on('connect')
+def handle_connect():
+    print('someone connected!')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('someone disconnected')
+
+@socketio.on('join')
+def handle_join(data):
+    room_id = data['room_id']
+    join_room(room_id)
+    print(f"user has joined room {room_id}")
+
 
 if __name__=="__main__":
     socketio.run(app, debug =True)
